c1af8bc6fea27cad11667dd7e7cad3ad9e4d534b
Amit Mahadik <amit.mahadik@gmail.com>
2026-01-23 22:15:28 -0800

    Initial Commit

---
+++ Astryx/AstryxApp.swift
@@ -0,0 +1,32 @@
+//
+//  AstryxApp.swift
+//  Astryx
+//
+//  Created by Mahadik, Amit on 1/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AstryxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AstryxTests/AstryxTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstryxTests.swift
+//  AstryxTests
+//
+//  Created by Mahadik, Amit on 1/23/26.
+//
+
+import Testing
+@testable import Astryx
+
+struct AstryxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
